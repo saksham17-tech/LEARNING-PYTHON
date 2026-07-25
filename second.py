e5a59b603761f23480eea2037b307565590db394
Saksham Mishra <msaksham771@gmail.com>
2026-07-25 22:06:27 +0530

    Variables in py

---
+++ second.py
@@ -0,0 +1,26 @@
+#VARIABLES IN PYTHON:-
+
+print()
+
+#strings
+name = "kenzy"
+food = "pizza"
+print("my name is",name)    #space is auto added
+print(f"my name is {name}")
+print(f"my fav food is {food}")
+
+print()
+
+#integers
+age = 19
+print(f"you are {age} years old\n")
+
+#float
+price = 19.99
+print(f"kit-kat is {price} rupees\n")
+
+#boolean
+is_Student = False
+print(f"are you a student?: {is_Student}")
+
+print("I am a student " if (is_Student) else "I am NOT a student")
