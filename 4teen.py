a1fd7979c2a1396850c372535dc3df16d6f6ed94
Saksham Mishra <msaksham771@gmail.com>
2026-07-26 23:51:33 +0530

    string indexing

---
+++ 4teen.py
@@ -0,0 +1,12 @@
+#string indexing
+
+credit_number = "1234-5678-9012-3456"
+
+print(credit_number[0])         #0-based indexing
+print(credit_number[:4])        #start substring
+print(credit_number[5:9])       #mid substring
+print(credit_number[5:])        #end substring
+print(credit_number[-1])        #from ending
+print(credit_number[::2])       #step
+print(credit_number[::-3])      #ending
+print(credit_number[::-1])      #reverse
