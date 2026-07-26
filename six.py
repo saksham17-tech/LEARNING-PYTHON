6179e0d31c5d333cf54bb16c60fbcad9211cb3e8
Saksham Mishra <msaksham771@gmail.com>
2026-07-26 18:45:22 +0530

    mad-libs game

---
+++ six.py
@@ -0,0 +1,12 @@
+#MADLIBS GAME
+
+adj1 = input(f"\nEnter an adjective (description): ")
+noun1 = input(f"Enter a noun (person/place/animal/thing): ")
+adj2 = input(f"Enter an adjective (description): ")
+verb = input(f"Enter a verb ending with -ing: ")
+adj3 = input(f"Enter an adjective (description): ")
+
+print(f"\nToday I went to a {adj1} zoo,")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adj2} and {verb},")
+print(f"I was {adj3}!")
