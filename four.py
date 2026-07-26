0bb53e89f886092f25f459bee84ee4b5dfa98d4f
Saksham Mishra <msaksham771@gmail.com>
2026-07-26 11:54:39 +0530

    input in py

---
+++ four.py
@@ -0,0 +1,7 @@
+#USER-INPUT IN PYTHON:
+
+name = input("\nWho is the MCU's Ghost Rider?\n")
+time = int(input("\nWhen will the movie come out?\n"))
+
+print(f"\nMCU's new Ghost Rider is {name}.")
+print(f"Marvel Studios' Ghost Rider will come out in {time}.")
