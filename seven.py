c97e694cff3c74651f8d5c5769c7b5080d9a8f7e
Saksham Mishra <msaksham771@gmail.com>
2026-07-26 19:15:24 +0530

    arithmetic and math functions

---
+++ seven.py
@@ -0,0 +1,27 @@
+#ARITHMETIC AND MATH
+import math
+
+avg = 59
+
+avg **= 13       #power
+print(avg)
+avg %= 31        #modulus
+print(avg)  
+avg //=3         #floor division
+print(avg)
+
+x = 14.563
+y = 13
+z = -4
+
+print(round(x))             #round func
+print(abs(z))               #absolute func
+print(pow(y,5))             #power func
+print(max(x,y,z))           #max func
+print(min(x,y,z))           #min func
+
+print(math.pi)
+print(math.e)
+print(math.sqrt(x))         #square root func
+print(math.ceil(x))         #ceiling func
+print(math.floor(x))        #floor func
