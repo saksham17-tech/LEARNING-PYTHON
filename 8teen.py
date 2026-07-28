43978aecbdaa4e39c3e481fc61db6b1ee270933a
Saksham Mishra <msaksham771@gmail.com>
2026-07-28 21:12:06 +0530

    nested loops

---
+++ 8teen.py
@@ -0,0 +1,11 @@
+#NESTED LOOPS
+
+
+rows = int(input("Enter the # of rows: "))
+columns = int(input("Enter the # of columns: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end=" ")
+    print()
